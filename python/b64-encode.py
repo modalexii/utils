a9d0b5a1714fe3c10b2cff030a0ffe05c3e5dd54
modalexii <modalexii@users.noreply.github.com>
2013-10-19 12:27:43 -0400

    Create b64-encode.py

---
+++ python/b64-encode.py
@@ -0,0 +1,16 @@
+# base-64 encode anything (images, binaries, etc) so it can be stored in a text document
+# run as b64envoce-py <infile.whatever> <outfile.txt>
+
+import base64
+import sys
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+with open(infile, "rb") as f:
+    orig_bin = f.read()
+    orig_b64 = orig_bin.encode("base64")
+
+fout = open(outfile,"wb")
+fout.write(orig_b64)
+fout.close()
