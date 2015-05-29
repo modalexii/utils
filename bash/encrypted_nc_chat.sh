5f9802e380ee1b39f11937002f02436987227c5c
modalexii <modalexii@users.noreply.github.com>
2015-05-28 23:32:34 -0400

    add tweet size version

--- bash/encrypted_nc_chat.sh
+++ bash/encrypted_nc_chat.sh
@@ -3,3 +3,13 @@ read -p "Enter Key:" key; read -p "Server address: " server; while true; do read
 
 # server:
 read -p "Enter Key: " key; while true; do read -n30 ui; echo $ui |openssl enc -aes-256-cbc -a -k $key; done | nc -l 8877 | while read so; do decoded_so=`echo "$so"| openssl enc -d -a -aes-256-cbc -k $key`; echo ">>> $decoded_so"; done
+
+# tweet-size version
+# this is *even more* of a novelty and has many more quirks
+# server expects password as first line, client expects password + space + server host
+
+# client"
+read k s;while true;do read u;openssl enc -a -bf -k $k<<<$u;done|nc $s 2|while read e;do d=`openssl enc -a -d -bf -k $k<<<$e`;echo \>$d;done
+
+# server:
+read k;while true;do read u;openssl enc -a -bf -k $k<<<$u;done|nc -l 2|while read e;do d=`openssl enc -a -d -bf -k $k<<<$e`;echo \>$d;done
